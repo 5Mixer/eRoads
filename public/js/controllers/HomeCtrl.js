7ced1ea60620cbd48ea3385e70b23da751774c38
5Mixer <dantheman41@live.com.au>
2016-07-09 14:18:05 +1000

    Watch name for changed

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,5 +1,8 @@
 angular.module('HomeCtrl', ['AccountService']).controller('HomeController', function($scope,Account) {
     $scope.Name = Account.getAccount().email;
+    $scope.$watch('Account.getAccount()',function(account){
+        $scope.Name = account.email;
+    });
     $scope.logout = Account.logout;
 
 });
